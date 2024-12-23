28446995b92f06d0a1f16d365b9db98fc35c17cd
Ralphy89 <43939986+ralphy89@users.noreply.github.com>
2024-12-22 21:13:00 -0500

    Update views.py and services.py

---
+++ ftp_management/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.core.files.storage import default_storage
+import os
+from . import services
+
+root = "C:\LAB-PROJECTs\\network_files_management\media\\"
+host_ = 'LAB-C113'
+password_ = 'LAB-C113'
+global DEVICES
+DEVICES = []
+# Create your views here.
+
+def home(request):
+    template = 'home.html'
+    global DEVICES
+    DEVICES = services.get_devices() # [['name', 'ip', 'mac'], ..., ...]
+    connected_devices = len(DEVICES)
+    page_object = {
+        'connected_devices': connected_devices,
+        'devices': DEVICES
+    }
+    assert template == 'home.html', 'template should be \"home.html\"'
+    return render(
+        request,
+        template,
+        page_object,
+    )
+
+def files_management(request):
+    global DEVICES
+
+    template = 'gestionnaire_fichiers/file_management.html'
+    if len(DEVICES) == 0:
+        DEVICES = services.get_devices()
+
+    page_object = {}
+    assert template == 'gestionnaire_fichiers/file_management.html', 'template should be \"gestionnaire_fichiers/file_management.html\"'
+    return render(
+        request,
+        template,
+        page_object,
+    )
+
+@csrf_exempt
+def upload_files(request):
+    if request.method == 'POST':
+        files = request.FILES.getlist('files[]')  # Access the array of files
+        saved_files = []
+        for file in files:
+            file_path = default_storage.save(f'uploads/{file.name}', file)
+            saved_files.append(f"{root}{file_path}")
+        print('Sharing files .... ')
+        transfer_files(saved_files)
+        return JsonResponse({'status': 'success', 'files': saved_files})
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method.'})
+
+@csrf_exempt
+def upload_directory(request):
+    saved_files = []
+    if request.method == 'POST':
+        # Retrieve the directory path from the request
+        directories_root = request.POST.getlist('dirs[]')
+        print(directories_root)
+        for directory_root in directories_root:
+            # Check if the directory exists and is valid
+            if not directory_root or not os.path.exists(directory_root) or not os.path.isdir(directory_root):
+                # return JsonResponse({'status': 'error', 'message': 'Invalid directory root.'})
+                continue
+            saved_files.append(directory_root)
+
+        transfer_files(saved_files, True)
+        return JsonResponse({'status': 'success', 'files': saved_files})
+
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method.'})
+
+
+
+def transfer_files(paths, r=False):
+    try:
+        for ip in DEVICES: # [['name', 'ip', 'mac'], ..., ...]
+            for path in paths:
+                # services.ssh_copy_file_to_remote_client(host_, ip[1], password_, path, r)
+                print(f"Sharing to {host_}@{ip[1]} : {path}")
+    except Exception as e:
+        print(f'Error sharing files : {e}')
+
+
