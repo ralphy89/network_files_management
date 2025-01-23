b67489d429aa058937ed7fab890f5cc3a4efe77a
Ralphy89 <43939986+ralphy89@users.noreply.github.com>
2025-01-23 14:14:58 -0500

    Now can search student by name, email and code!
    Signed-off-by: Ralphy89 <43939986+ralphy89@users.noreply.github.com>

--- ftp_management/services.py
+++ ftp_management/services.py
@@ -66,6 +66,9 @@ def ssh_copy_file_to_remote_client(host: str, ip: str, password: str, file: str,
             scp.put(file, f'./{default_dest_folder}/', r)  # Upload file to the remote directory
 
         print(f"File '{file}' copied successfully to {ip}:./{default_dest_folder}/")
+        client.close()
+        print("SSH connection closed.\n")
+        return ip
 
     except Exception as e:
         print(f"Error : {e}")
